6c273c4e54ce4ce12f6a1e7b28a7f43bc9de2033
Administrator <Administrator@192.168.20.60>
2019-10-07 17:49:03 +0200

    afternoon 7th october

---
+++ WebContent/getVAM.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+The height difference is: <?php echo $_POST["HeightDiff"]; ?><br>
+The time is: <?php echo $_POST["Time"]; ?>
+
+</body>
+</html>
---
+++ WebContent/js/getVAM.js
@@ -0,0 +1,3 @@
+let header = document.querySelector('h2');
+header.textContent = 'VAM Calculator!';
+console.log('The VAM is:!');
